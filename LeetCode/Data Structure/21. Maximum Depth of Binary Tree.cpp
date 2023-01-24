ef86405ddae99a7a30f27c5988cc582f60a8f227
madiv9820 <themathur901@gmail.com>
2023-01-24 20:41:35 +0530

    Max Depth of BTree Code

---
+++ LeetCode/Data Structure/21. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,71 @@
+/*
+Maximum Depth of Binary Tree
+
+Given the root of a binary tree, return its maximum depth.
+A binary tree's maximum depth is the number of nodes along 
+the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+
+Constraints:
+
+    The number of nodes in the tree is in the range [0, 104].
+    -100 <= Node.val <= 100
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* takeInput() {
+    TreeNode *root = NULL;
+    int n,left, right;
+    cin >> n;
+    if(n == -1)
+        return root;
+    root = new TreeNode(n);
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()) {
+        TreeNode* front = q.front();
+        q.pop();
+        cin >> left >> right;
+        if(left != -1) {
+            TreeNode* leftT = new TreeNode(left);
+            front->left = leftT;
+            q.push(leftT);
+        }
+        if(right != -1) {
+            TreeNode* rightT = new TreeNode(right);
+            front->right = rightT;
+            q.push(rightT);
+        }
+    }
+    return root;
+}
+
+int maxDepth(TreeNode* root) {
+    if(root == NULL)
+        return 0;
+    return 1+max(maxDepth(root->left),maxDepth(root->right));
+}
+
+int main() {
+    TreeNode* root = takeInput();
+    cout << maxDepth(root) << endl;
+}
