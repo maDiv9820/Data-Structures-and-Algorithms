a95cb11a946cc9c581a99a8bfb8c9debf1f7699c
madiv9820 <themathur901@gmail.com>
2023-01-25 19:18:29 +0530

    Path Sum Code

---
+++ LeetCode/Data Structure/24. Path Sum.cpp
@@ -0,0 +1,87 @@
+/*
+Path Sum
+
+Given the root of a binary tree and an integer targetSum, 
+return true if the tree has a root-to-leaf path such that adding 
+up all the values along the path equals targetSum.
+A leaf is a node with no children.
+
+Example 1:
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+
+Example 2:
+
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+
+Example 3:
+
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+
+Constraints:
+
+    The number of nodes in the tree is in the range [0, 5000].
+    -1000 <= Node.val <= 1000
+    -1000 <= targetSum <= 1000
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+TreeNode* takeInput() {
+    TreeNode *root = NULL;
+    int n,left, right;
+    cin >> n;
+    if(n == -1)
+        return root;
+    root = new TreeNode(n);
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()) {
+        TreeNode* front = q.front();
+        q.pop();
+        cin >> left >> right;
+        if(left != -1) {
+            TreeNode* leftT = new TreeNode(left);
+            front->left = leftT;
+            q.push(leftT);
+        }
+        if(right != -1) {
+            TreeNode* rightT = new TreeNode(right);
+            front->right = rightT;
+            q.push(rightT);
+        }
+    }
+    return root;
+}
+bool hasPathSum(TreeNode* root, int targetSum) {
+    if(root == NULL)
+        return false;
+    if(!root->left && !root->right && targetSum-root->val == 0)
+        return true;
+    if(hasPathSum(root->left,targetSum-root->val))
+        return true;
+    return hasPathSum(root->right,targetSum-root->val);
+}
+int main() {
+    TreeNode* root = takeInput();
+    int targetSum;
+    cin >> targetSum;
+    cout << ((hasPathSum(root,targetSum) == 1) ? "true":"false") << endl;
+}
