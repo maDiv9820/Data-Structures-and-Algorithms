b5bfee70304277a81516edca91e3da081ff1887e
madiv9820 <themathur901@gmail.com>
2023-02-24 13:58:50 +0530

    Minimize Deviation in an Array

---
+++ LeetCode/Daily/04. Minimize Deviation in an Array.cpp
@@ -0,0 +1,80 @@
+/*
+Minimize Deviation in Array
+
+You are given an array nums of n positive integers.
+You can perform two types of operations on any element 
+of the array any number of times:
+
+    If the element is even, divide it by 2.
+        For example, if the array is [1,2,3,4], then you can do this operation on the last element, and the array will be [1,2,3,2].
+    If the element is odd, multiply it by 2.
+        For example, if the array is [1,2,3,4], then you can do this operation on the first element, and the array will be [2,2,3,4].
+
+The deviation of the array is the maximum difference between any two elements in the array.
+
+Return the minimum deviation the array can have after performing some number of operations.
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: 1
+Explanation: You can transform the array to [1,2,3,2], then to [2,2,3,2], then the deviation will be 3 - 2 = 1.
+
+Example 2:
+
+Input: nums = [4,1,5,20,3]
+Output: 3
+Explanation: You can transform the array after two operations to [4,2,5,5,3], then the deviation will be 5 - 2 = 3.
+
+Example 3:
+
+Input: nums = [2,10,8]
+Output: 3
+
+Constraints:
+
+    n == nums.length
+    2 <= n <= 5 * 104
+    1 <= nums[i] <= 109
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int minimumDeviation(vector<int>& nums) {
+    priority_queue<int> pq;
+    int minVal = INT_MAX;
+    for (int num : nums) {
+        if (num % 2 == 1)
+            num = num * 2;
+        pq.push(num);
+        minVal = min(minVal, num);
+    }
+    int minDeviation = INT_MAX;
+    while (true) {
+        int maxVal = pq.top();
+        pq.pop();
+        minDeviation = min(minDeviation, maxVal - minVal);
+        /*
+            The reason we need to break out of the loop when the maximum value 
+            is odd is that we have already transformed all odd numbers in the input
+            array to even numbers by multiplying them by 2. Therefore, if the maximum 
+            value in the priority queue is odd, it must have been obtained by performing 
+            the "multiply by 2" operation on some even number. We cannot undo this operation 
+            by performing the "divide by 2" operation, so we cannot reduce the maximum valu 
+            any further.
+        */
+        if (maxVal % 2 == 1)
+            break;
+        maxVal = maxVal / 2;
+        minVal = min(minVal, maxVal);
+        pq.push(maxVal);
+    }
+    return minDeviation;
+}
+int main() {
+    int n;
+    cin >> n;
+    vector<int> nums(n,0);
+    for(int index = 0; index < n; index++)
+        cin >> nums[index];
+    cout << minimumDeviation(nums) << endl;
+}
