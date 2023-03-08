cc8e498692dddff1e15031ca56887d57ed1e3a62
madiv9820 <themathur901@gmail.com>
2023-03-08 19:54:39 +0530

    Search in Rotated Sorted Array

---
+++ LeetCode/Daily/Python/15. Search in Sorted Rotated Array.py
@@ -0,0 +1,57 @@
+'''
+Search in Rotated Sorted Array II
+
+There is an integer array nums sorted in non-decreasing order 
+(not necessarily with distinct values). Before being passed to your function, 
+nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that 
+the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], 
+..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated 
+at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4]. Given the array nums after the 
+rotation and an integer target, return true if target is in nums, or false if it 
+is not in nums. You must decrease the overall operation steps as much as possible.
+
+Example 1:
+
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
+
+Example 2:
+
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
+
+Constraints:
+
+    1 <= nums.length <= 5000
+    -10^4 <= nums[i] <= 10^4
+    nums is guaranteed to be rotated at some pivot.
+    -10^4 <= target <= 10^4
+'''
+class Solution:
+    def __bSearch(self, nums: list[int], start: int, end: int, target: int) -> bool:
+        while start <= end:
+            mid = (start+end)//2
+            if nums[mid] == target:
+                return True
+            elif nums[mid] > target:
+                end = mid-1
+            else:
+                start = mid+1
+        return False
+    
+    def search(self, nums: list[int], target: int) -> bool:
+        pivot = -1
+        for index in range(len(nums)):
+            if nums[index] == target:
+                return True
+            if index+1 < len(nums) and nums[index] > nums[index+1]:
+                pivot = index
+                break
+        if pivot == -1:
+            return False
+        return self.__bSearch(nums,pivot+1,len(nums)-1,target)
+        
+nums = [int(x) for x in input().split()]
+target = int(input())
+sol = Solution()
+print("true") if sol.search(nums,target) else print("false")
